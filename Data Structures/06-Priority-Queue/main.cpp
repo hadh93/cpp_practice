0350c501216d3f3fdeb25fbaf0c66a3cbc2af898
hadh93 <hoondog1993@gmail.com>
2024-05-09 23:54:50 +0900

    Modified priority queue as per SWEA reference

--- Data Structures/06-Priority-Queue/main.cpp
+++ Data Structures/06-Priority-Queue/main.cpp
@@ -6,59 +6,62 @@
 #include <iostream>
 using namespace std;
 
-struct item {
-    int value;
-    int priority;
-};
+#define MAX_SIZE 100
 
-item pr[100000];
+int heap[MAX_SIZE];
+int heapSize = 0;
 
-int pq_size = -1;
-
-void enqueue(int value, int priority){
-    pq_size++;
-    pr[pq_size].value = value;
-    pr[pq_size].priority = priority;
+void heapInit(){
+    heapSize = 0;
 }
 
-int peak(){
-    int highestPriority = INT_MIN;
-    int ind = -1;
+int heapPush(int value){
+    if (heapSize+1 > MAX_SIZE){
+        cout << "queue is full!" << endl;
+        return 0;
+    }
+    heap[heapSize] = value;
+    int current = heapSize;
+    while (current>0 && heap[current] < heap[(current-1)/2]){
+        int temp = heap[(current-1) / 2];
+        heap[(current-1)/2]  = heap[current];
+        heap[current] = temp;
+        current = (current-1) / 2;
+    }
+    heapSize = heapSize +1;
+    return 1;
+}
 
-    for (int i=0; i <= pq_size; i++){
-        if (highestPriority == pr[i].priority && ind >-1 && pr[ind].value < pr[i].value){
-            highestPriority = pr[i].priority;
-            ind = i;
-        } else if (highestPriority < pr[i].priority){
-            highestPriority = pr[i].priority;
-            ind = i;
+int heapPop(int *value){
+    if (heapSize <= 0){
+        return -1;
+    }
+    *value = hepa[0];
+    heapSize = heapSize-1;
+
+    heap[0] = heap[heapSize];
+
+    int current = 0;
+    while (current * 2 +1 < heapSize){
+        int child;
+        if (current*2+2 == heapSize){
+            child = current*2 +1;
+        } else{
+            child = heap[current*2+1] < heap[current*2+2] ? current * 2 + 1 : current*2 + 2;
         }
-    }
-    return ind;
-}
 
-void dequeue(){
-    int ind = peak();
-    for (int i = ind; i < pq_size; i++){
-        pr[i] = pr[i+1];
-    }
-    pq_size--;
-}
+        if (heap[current] < heap[child]){
+            break;
+        }
+        int temp = heap[current];
+        heap[current] = heap[child];
+        heap[child] = temp;
 
+        current = child;
+    }
+    return 1;
+}
 
 int main(){
-    enqueue(10,2);
-    enqueue(14,4);
-    enqueue(16,4);
-    enqueue(12,3);
-
-    int ind = peak();
-    cout << pr[ind].value << endl;
-
-    dequeue();
-
-    ind = peak();
-    cout << pr[ind].value << endl;
-
     return 0;
 }
